ff812acb0122d08bbdbc81bc47d59fbc895e0c58
Maqsood <maqsoodansari818@gmail.com>
2021-12-26 10:08:37 +0530

    firstcommit

---
+++ src/main/java/com/maqcoding/sbmongomapppingembedded/SbMongoMapppingEmbeddedApplication.java
@@ -0,0 +1,13 @@
+package com.maqcoding.sbmongomapppingembedded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMongoMapppingEmbeddedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMongoMapppingEmbeddedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maqcoding/sbmongomapppingembedded/SbMongoMapppingEmbeddedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maqcoding.sbmongomapppingembedded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMongoMapppingEmbeddedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
